d77e508b6ba91466e4081d1faffddb51f65c7df1
JFank612 <36108855+JFank612@users.noreply.github.com>
2020-02-03 16:28:03 -0600

    Add files via upload

---
+++ Variables.kt
@@ -0,0 +1,23 @@
+fun main(arg: Array<String>) {
+
+    var byte = 2
+    var short = 4
+    var int = 6
+    var long = 1000001L
+    var float = 8.674F
+    var double = 88.88888888
+    var boolean:Boolean = false
+    val letter:Char
+    letter = 'Q'
+
+    println("Byte:" +byte)
+    println("Short:" +short)
+    println("Integer:" +int)
+    println("Long:" +long)
+    println("Float:" +float)
+    println("Double:" +double)
+    println("Boolean:" +boolean)
+    println("Character:" +letter)
+
+
+}
